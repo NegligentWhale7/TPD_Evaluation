2580c6937a6f8442ca63ff2c7b6c163d02c484c9
NegligentWhale7 <carlosalam33@hotmail.com>
2024-10-10 16:28:36 -0600

    AI Progress

--- Assets/_Master/Scripts/Enemies/Enemy.cs
+++ Assets/_Master/Scripts/Enemies/Enemy.cs
@@ -6,8 +6,13 @@ using UnityEngine.AI;
 [RequireComponent(typeof(NavMeshAgent))]
 public class Enemy : MonoBehaviour
 {
+    [SerializeField] ObjectPooler bulletPool;
     [SerializeField] NavMeshAgent agent;
+    [SerializeField] Transform firePoint;
+    [SerializeField] float bulletSpeed;
+    [SerializeField] float timeForShoot;
     Transform player;
+    float timeCounter = 0;
 
     private void Awake()
     {
@@ -17,8 +22,29 @@ public class Enemy : MonoBehaviour
 
     private void FixedUpdate()
     {
-        if (player == null) return;
-        
+        if (player == null) return;        
         agent.destination = player.position;
+
+        timeCounter += Time.deltaTime;
+        if (timeCounter > timeForShoot)
+        {
+            ShootBullet();
+            timeCounter = 0;
+        }
+    }
+
+    private void ShootBullet()
+    {
+        GameObject bullet = bulletPool.GetPooledObject();
+        if (bullet != null)
+        {
+            bullet.transform.position = firePoint.position;
+            bullet.transform.rotation = firePoint.rotation;
+            bullet.SetActive(true);  // Activar la bala
+
+            // Aplicar la velocidad de la bala
+            Rigidbody rb = bullet.GetComponent<Rigidbody>();
+            rb.velocity = firePoint.forward * bulletSpeed;
+        }
     }
 }
